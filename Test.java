7490834af354397991de472c3f693f894800d3e0
Aditya rana <adityarana1140@gmail.com>
2025-08-16 15:23:28 +0530

    This is main method multithreading

---
+++ Test.java
@@ -0,0 +1,7 @@
+class Test{
+    public static void main(String[] args) { // This is main thread when we start the main method the 
+        // main thread starts executing
+        // This is the entry point of the Java application
+        System.out.println("Hello, World!"); 
+    }
+}
